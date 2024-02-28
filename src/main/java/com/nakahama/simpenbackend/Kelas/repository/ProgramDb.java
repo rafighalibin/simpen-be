68c0ccdb990a0500aa8ba2f9cfc1d31c7eff5cb3
Nadhif Rahman Alfan <nadhif.rahman11@ui.ac.id>
2024-02-28 21:53:50 +0700

    Nadhif.rahman11/feature/pbi 5/initiate

---
+++ src/main/java/com/nakahama/simpenbackend/Kelas/repository/ProgramDb.java
@@ -0,0 +1,20 @@
+package com.nakahama.simpenbackend.Kelas.repository;
+
+import com.nakahama.simpenbackend.Kelas.model.*;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@Transactional
+public interface ProgramDb extends JpaRepository<Program, UUID>{
+
+    Optional<Program> findById(UUID id);
+
+    Optional<Program> findByNama(String nama);
+}
