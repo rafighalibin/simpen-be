af438e59472381eb04fee101d67864aa6ddd5db6
rafighalibin <rafighalibin@ui.ac.id>
2024-03-05 10:34:19 +0700

    [REFACTOR] User package

--- src/main/java/com/nakahama/simpenbackend/User/dto/User/EditUserRequestDTO.java
+++ src/main/java/com/nakahama/simpenbackend/User/dto/User/EditUserRequestDTO.java
@@ -1,19 +1,37 @@
 package com.nakahama.simpenbackend.User.dto.User;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
-@Getter
+@Data
 @Setter
-@NoArgsConstructor
+@Getter
+@Builder
 @AllArgsConstructor
-public class EditUserRequestDTO extends AddUserRequestDTO {
+@NoArgsConstructor
+public class EditUserRequestDTO {
+    // assigned at controller
     UUID id;
+
+    @NotBlank(message = "Nama required")
+    String nama;
+
+    @NotBlank(message = "Email required")
+    String email;
+
+    @NotBlank(message = "Password required")
     String password;
+
+    @NotBlank(message = "Jenis Kelamin required")
     String jenisKelamin;
+
+    @NotBlank(message = "Nomor Telepon required")
     String noTelp;
 }
