94214c29a9c728816cf3f8a6f49717de32357c85
Kristo Jeremy Thady Tobing <kristo.jeremy@ui.ac.id>
2024-03-19 16:51:00 +0700

    Kristo/feature/pbi 1/create user

--- src/main/java/com/nakahama/simpenbackend/User/service/UserServiceImpl.java
+++ src/main/java/com/nakahama/simpenbackend/User/service/UserServiceImpl.java
@@ -5,18 +5,22 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
 import java.util.Comparator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.nakahama.simpenbackend.User.dto.Tag.ReadTagResponseDTO;
 import com.nakahama.simpenbackend.User.dto.User.EditDataPengajarRequestDTO;
 import com.nakahama.simpenbackend.User.dto.User.EditUserRequestDTO;
+import com.nakahama.simpenbackend.User.dto.User.UserWithTagsResponseDTO;
 import com.nakahama.simpenbackend.User.dto.User.mapper.PengajarMapper;
 import com.nakahama.simpenbackend.User.model.Akademik;
 import com.nakahama.simpenbackend.User.model.Operasional;
 import com.nakahama.simpenbackend.User.model.Pengajar;
+import com.nakahama.simpenbackend.User.model.Tag;
 import com.nakahama.simpenbackend.User.model.UserModel;
 import com.nakahama.simpenbackend.User.repository.AkademikDb;
 import com.nakahama.simpenbackend.User.repository.OperasionalDb;
@@ -173,20 +177,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserModel updateUser(EditUserRequestDTO editUserRequestDTO) {
         UserModel user = getUserById(editUserRequestDTO.getId());
+
         if (editUserRequestDTO.getNama() != null) {
             user.setNama(editUserRequestDTO.getNama());
         }
-        if (editUserRequestDTO.getEmail() != null) {
-            user.setEmail(editUserRequestDTO.getEmail());
-        }
-        if (editUserRequestDTO.getPassword() != null) {
-            user.setPassword(bCryptPasswordEncoder.encode(editUserRequestDTO.getPassword()));
-        }
-        if (editUserRequestDTO.getJenisKelamin() != null) {
-            user.setJenisKelamin(editUserRequestDTO.getJenisKelamin());
-        }
-        if (editUserRequestDTO.getNoTelp() != null) {
-            user.setNoTelp(editUserRequestDTO.getNoTelp());
+        if (editUserRequestDTO.getEmail() != null && !user.getEmail().equals(editUserRequestDTO.getEmail())) {
+            if (!isExistByEmail(editUserRequestDTO.getEmail())) {
+                user.setEmail(editUserRequestDTO.getEmail());
+            } else {
+                return null;
+            }
+        } else {
+            editUserRequestDTO.getEmail();
         }
 
         userDb.save(user);
@@ -196,12 +198,40 @@ public class UserServiceImpl implements UserService {
     @Override
     public Pengajar editDataPengajar(EditDataPengajarRequestDTO pengajarRequestDTO) {
         Pengajar pengajar = pengajarDb.findById(pengajarRequestDTO.getId())
-                .orElseThrow(() -> new BadRequestException("Pengajar with id " + pengajarRequestDTO.getId() + " not found"));
-        
+                .orElseThrow(
+                        () -> new BadRequestException("Pengajar with id " + pengajarRequestDTO.getId() + " not found"));
+
         pengajar = PengajarMapper.toEntity(pengajarRequestDTO, pengajar);
 
         pengajarDb.save(pengajar);
         return pengajar;
+    }
+
+    @Override
+    public UserWithTagsResponseDTO getUserAndTag(UUID id) {
+        UserModel user = getUserById(id);
+        UserWithTagsResponseDTO response = new UserWithTagsResponseDTO();
+
+        if (user instanceof Pengajar) {
+            Pengajar pengajar = (Pengajar) user;
+            List<Tag> tags = pengajar.getListTag();
+
+            List<ReadTagResponseDTO> tagsDTOs = new ArrayList<>();
+            for (Tag tag : tags) {
+                tagsDTOs.add(new ReadTagResponseDTO(tag.getId(), tag.getNama()));
+            }
+
+            response.setTags(tagsDTOs);
+        }
+
+        response.setId(id);
+        response.setNama(user.getNama());
+        response.setEmail(user.getEmail());
+        response.setEmailPribadi(user.getEmailPribadi());
+        response.setJenisKelamin(user.getJenisKelamin());
+        response.setRole(user.getRole());
+
+        return response;
     }
 
 }
